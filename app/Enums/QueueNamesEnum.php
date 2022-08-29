b061d17ac630a6b96b45205e7d10770bf3dfa9ae
Aleksandr Mitin <mitinsoft@gmail.com>
2022-08-30 00:02:21 +0300

    current

--- app/Enums/QueueNamesEnum.php
+++ app/Enums/QueueNamesEnum.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MitinSany\PushCommon\App\Enums\AMQPService;
+namespace MitinSany\PushCommon\App\Enums;
 
 class QueueNamesEnum
 {
---
+++ app/Enums/ResponseResultEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MitinSany\PushCommon\App\Enums;
+
+class ResponseResultEnum
+{
+    public const STATE_PROCESSED = 'processed';
+    public const STATE_FAILED = 'failed';
+}
